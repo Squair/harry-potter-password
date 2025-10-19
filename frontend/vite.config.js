7ba9bea4af02ed66f30715f59f92468a868a903d
squair <georgesquair@gmail.com>
2025-10-19 17:59:00 +0100

    Add base to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/harry-potter-password/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
